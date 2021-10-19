deb845d1f3656818ced536ca61490d93cdee68fc
Mohammed Shoaib <mdshoaib093@gmail.com>
2021-10-19 11:41:05 +0530

    Initial commit

---
+++ src/app/modal.ts
@@ -0,0 +1,14 @@
+export interface datecount{
+    datetime:String,
+    count:number
+}
+export interface url{
+    id?:number,
+    longURL:string,
+    short:string,
+    count:number,
+}
+export interface urlChart{
+    name:String,
+    value:number
+}
